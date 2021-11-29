9a51c062c607fc0e2b8bf489243c417d77367a17
Abdur Rahman BIn Muhammad <arshadhinjr@gmail.com>
2021-11-29 23:22:37 +0600

    commit navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <h1>Abdur Rahman's Portfolio is comming soon. Well</h1>
+
     </div>
   );
 }
