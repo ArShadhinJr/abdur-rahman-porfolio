e33d155e0bdaae32c57425016d286fa024fab661
Abdur Rahman BIn Muhammad <arshadhinjr@gmail.com>
2021-11-29 23:46:17 +0600

    update navbar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,31 +1,29 @@
 import React from 'react';
-import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
     return (
         <Navbar bg="info" expand="lg">
-            <Container className="" fluid="md">
-                <Row className="mb-0 pb-0">
-                    <Col md={4} className="mb-0 pb-0">
-                        <Navbar.Brand className="mb-0 pb-0 text-white text-uppercase fw-bolder" href="#"><h2 className="mb-0 pb-0">abdur rahman</h2></Navbar.Brand>
-                    </Col>
-                    <Col md={8} className="mb-0 pb-0">
-                        <Navbar.Toggle aria-controls="navbarScroll" />
-                        <Navbar.Collapse id="navbarScroll">
-                            <Nav
-                                className="me-auto my-2 my-lg-0"
-                                style={{ maxHeight: '100px' }}
-                                navbarScroll
-                            >
-                                <Nav.Link href="#action1">ABOUT ME</Nav.Link>
-                                <Nav.Link href="#action2">PROJECTS</Nav.Link>
-                                <Nav.Link href="#action2">HAIR ME</Nav.Link>
-                                <Nav.Link href="#action2">CONTACT</Nav.Link>
-                                <Nav.Link className="btn btn-light" href="#action2">RESUME</Nav.Link>
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Col>
-                </Row>
+            <Container className="d-flex justify-content-between" fluid>
+                <Container flluid>
+                    <Navbar.Brand className="mb-0 pb-0 text-white text-uppercase fw-bolder" href="#">ABdur Rahman</Navbar.Brand>
+                </Container>
+                <Container fluid className="d-flex justify-content-end">
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="me-auto my-2 my-lg-0 d-flex justify-content-end"
+                            style={{ maxHeight: '100px' }}
+                            navbarScroll
+                        >
+                            <Nav.Link href="#action1">ABOUT ME</Nav.Link>
+                            <Nav.Link href="#action2">PROJECTS</Nav.Link>
+                            <Nav.Link href="#action2">HAIR ME</Nav.Link>
+                            <Nav.Link href="#action2">CONTACT</Nav.Link>
+                            <Nav.Link className="btn btn-light" href="https://drive.google.com/file/d/1MvOa40LRLVSRCamXWN7tLJuLxmCzD1u1/view">RESUME</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
             </Container>
         </Navbar>
     );
