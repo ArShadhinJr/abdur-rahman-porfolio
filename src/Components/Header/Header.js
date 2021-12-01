ed9a2034d4f13c9397617185b8af0c3ecb556b5f
Abdur Rahman BIn Muhammad <arshadhinjr@gmail.com>
2021-12-01 10:14:18 +0600

    navbar updated

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,29 +3,29 @@ import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
     return (
-        <Navbar bg="info" expand="lg">
-            <Container className="d-flex justify-content-between" fluid>
-                <Container flluid>
+        <><Navbar bg="info" expand="lg">
+            <Container className="d-flex " fluid="md">
+                <div flluid>
                     <Navbar.Brand className="mb-0 pb-0 text-white text-uppercase fw-bolder" href="#">ABdur Rahman</Navbar.Brand>
-                </Container>
-                <Container fluid className="d-flex justify-content-end">
+                </div>
+                <div>
                     <Navbar.Toggle aria-controls="navbarScroll" />
                     <Navbar.Collapse id="navbarScroll">
                         <Nav
-                            className="me-auto my-2 my-lg-0 d-flex justify-content-end"
-                            style={{ maxHeight: '100px' }}
+                            className="me-auto my-2 my-lg-0"
+                            style={{ maxHeight: '200px' }}
                             navbarScroll
                         >
-                            <Nav.Link href="#action1">ABOUT ME</Nav.Link>
-                            <Nav.Link href="#action2">PROJECTS</Nav.Link>
-                            <Nav.Link href="#action2">HAIR ME</Nav.Link>
-                            <Nav.Link href="#action2">CONTACT</Nav.Link>
+                            <Nav.Link className="btn" href="#action1">ABOUT ME</Nav.Link>
+                            <Nav.Link className="btn" href="#action2">PROJECTS</Nav.Link>
+                            <Nav.Link className="btn" href="#action3">BLOGS</Nav.Link>
+                            <Nav.Link className="btn" href="#action4">CONTACT</Nav.Link>
                             <Nav.Link className="btn btn-light" href="https://drive.google.com/file/d/1MvOa40LRLVSRCamXWN7tLJuLxmCzD1u1/view">RESUME</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
-                </Container>
+                </div>
             </Container>
-        </Navbar>
+        </Navbar></> 
     );
 };
 
