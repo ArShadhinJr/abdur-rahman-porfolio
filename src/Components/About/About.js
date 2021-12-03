566cdb635d0698746a2f1082a4ccf5765ab6382d
Abdur Rahman BIn Muhammad <arshadhinjr@gmail.com>
2021-12-03 23:44:53 +0600

    commited

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,7 +1,5 @@
-import Button from '@restart/ui/esm/Button';
 import React from 'react';
 import { Card, Col, Container, Row } from 'react-bootstrap';
-import TypewriterSlug from '../Typewriter/TypewriterSlug';
 
 const About = () => {
     return (
@@ -11,7 +9,7 @@ const About = () => {
             <h2 className="mb-0 pb-0 text-info text-uppercase fw-bolder fs-2 nav-heading pt-4" href="#"><span className="fs-1 text-black-50">A</span>bout <span className="fs-1 text-black-50">M</span>e</h2>
                 <Row xs={1} md={2}>
                     
-                    <Col className=" text-start d-flex align-items-center font-monospace bg-custom border border-5 rounded rounded-3 my-5 my-xm-2 px-xm-2">
+                    <Col className=" text-start d-flex align-items-center font-monospace bg-custom border border-5 rounded rounded-3 my-5 my-xm-2 mx-xm-2">
                         <div >
                             <p>I'm Abdur Rahman. I'm a comfortable developer of MERN stack and forever learner of Javascript. I want to be a extreme MERN developer. </p>
                             <p>I am a JavaScript developer. I have worked with HTML, CSS, JavaScript, react Js, express Js, node Js, MongoDB with some necessary tools like git, GitHub, Firebase, Heroku, Netlify etc. I mostly love to work with JavaScript.</p>
