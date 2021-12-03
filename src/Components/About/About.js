bf2a41bc46f63b399ba75d86bf82e4d03652a45b
Abdur Rahman BIn Muhammad <arshadhinjr@gmail.com>
2021-12-03 20:05:00 +0600

    about me added

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -11,11 +11,12 @@ const About = () => {
             <h2 className="mb-0 pb-0 text-info text-uppercase fw-bolder fs-2 nav-heading pt-4" href="#"><span className="fs-1 text-black-50">A</span>bout <span className="fs-1 text-black-50">M</span>e</h2>
                 <Row xs={1} md={2}>
                     
-                    <Col className=" text-start d-flex align-items-center font-monospace bg-custom border border-5 rounded rounded-3 my-5 my-xm-2">
+                    <Col className=" text-start d-flex align-items-center font-monospace bg-custom border border-5 rounded rounded-3 my-5 my-xm-2 px-xm-2">
                         <div >
                             <p>I'm Abdur Rahman. I'm a comfortable developer of MERN stack and forever learner of Javascript. I want to be a extreme MERN developer. </p>
                             <p>I am a JavaScript developer. I have worked with HTML, CSS, JavaScript, react Js, express Js, node Js, MongoDB with some necessary tools like git, GitHub, Firebase, Heroku, Netlify etc. I mostly love to work with JavaScript.</p>
                             <p>I would like to go from beginner to advance in this stack periodically. I'm really looking forward to working with Web site security in the future.</p>
+                            <p>I love playing puzzle games. I am a sub'30 player of Rubik's Cube. I solve sudoku quite well. I am Tengram Lover.</p>
                         </div>
                     </Col>
                     <Col><Card.Img variant="top" className=" px-xs-2 py-xm-1" src="https://i.ibb.co/VJDMZpD/webdev1.png" /></Col>
